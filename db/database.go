a2be32830ec7a67c143f9d634c2944483ab429b0
ananyab1909 <bananya1909@gmail.com>
2025-06-20 13:54:36 +0530

    postgres in neon db successful

--- db/database.go
+++ db/database.go
@@ -1,9 +1,7 @@
 package db
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,15 +10,12 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),
-	)
+	dsn := "neon-db-connection-string"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
+
 	DB = database
 	log.Println("Connected to database")
 }
--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"log"
 	"net/http"
 
-	"custom-api-server/config"
 	"custom-api-server/db"
 )
 
 func main() {
-	config.LoadEnv()
 	db.Connect()
 
-	log.Println("Server is running on port", config.GetEnv("PORT", "8080"))
-	http.ListenAndServe(":"+config.GetEnv("PORT", "8080"), nil)
+	log.Println(" Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
